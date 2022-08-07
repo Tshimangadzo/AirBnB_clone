4e748bba1977558e6fdddc6b38c9c89f911eb600
Thavhat <tsiwant@telkom.co.za>
2022-08-07 09:15:07 +0200

    finished task 4

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="models",
+    version="1.0.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
